78b3fc7b7d99fb8d88ae8fe4115bcfa5ccca2dd6
amykeonucamp <amy@nucamp.co>
2020-06-04 22:08:34 -0700

    Instructor comments

--- src/components/CampsiteInfo.js
+++ src/components/CampsiteInfo.js
@@ -20,14 +20,23 @@ class CampsiteInfo extends Component {
 					<h4>Comments</h4>
 
 					{comments.map((comment) => {
-       				   return (
-       				    <div>
-       				      <p>{comment.text}</p>
-						  <p>{comment.author}</p>
-						  {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
-         			    </div>
-          			    );
-        			})}
+						return (
+							<div>
+								<p>{comment.text}</p>
+
+								<p>
+									{comment.author}
+									{/* Amy Instructor: move the date code inside this paragraph. */}
+								</p>
+
+								{new Intl.DateTimeFormat("en-US", {
+									year: "numeric",
+									month: "short",
+									day: "2-digit",
+								}).format(new Date(Date.parse(comment.date)))}
+							</div>
+						);
+					})}
 				</div>
 			);
 		}
